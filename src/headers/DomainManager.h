628ac96582dbcea54c85f3b099b968d25f5af72f
Abdullah Al Amin <nirjhor18@gmail.com>
2022-07-05 11:44:39 -0500

    time dependent step size and domain size

--- src/headers/DomainManager.h
+++ src/headers/DomainManager.h
@@ -80,9 +80,9 @@ class DomainManager
 };
 
         // global maximum node numbers (for performance)
-        const int _nx_ = 2000;
-        const int _ny_ = 1500;
-        const int _nz_ = 150;
+        const int _nx_ = 400;
+        const int _ny_ = 300;
+        const int _nz_ = 700;
 
 #endif
 
